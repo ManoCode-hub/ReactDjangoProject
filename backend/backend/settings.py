53d422fbec761d4d457d3fe1bff82025e211b6fb
Manoj M <marianeasonmanoj968@gmail.com>
2025-04-07 00:13:32 +0530

    frontend react

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -154,7 +154,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-CORS_ALLOWED_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWS_CREDENTIALS = True
